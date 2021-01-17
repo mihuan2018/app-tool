b751ebf98fb5f3c595e17744d5f768e5eeaee9f1
447130707@qq.com <447130707@qq.com>
2021-01-17 15:46:41 +0800

    20210117
    新增授权页，登录页，新增相关路由

---
+++ src/components/authorize.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="authorizer-wrapper">
+        <div class="authorizer-loading">
+            <i class="el-icon-loading"></i>
+            <div style="margin-top: 1rem;">正在授权</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        created(options) {
+
+        },
+        mounted(options) {
+            console.log(this.$route)
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped="scoped">
+    .authorizer-wrapper {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+    .authorizer-wrapper .authorizer-loading {
+        text-align: center;
+    }
+    .el-icon-loading {
+        font-size: 1.5rem;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,25 +6,34 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/', 
-      name: 'index', 
+      path: '/',
+      name: 'index',
       component:resolve => require(['@/components/index'],resolve)
     },
     {
-      path: '/list', 
-      name: 'list', 
+      path: '/list',
+      name: 'list',
       component:resolve => require(['@/components/list'],resolve)
     },
     {
-      path: '/setting', 
-      name: 'setting', 
+      path: '/setting',
+      name: 'setting',
       component:resolve => require(['@/components/setting'],resolve)
     },
     {
-      path: '/edit', 
-      name: 'edit', 
+      path: '/edit',
+      name: 'edit',
       component:resolve => require(['@/components/edit'],resolve)
     },
-
+    {
+        path: "/authorize",
+        name: "authorize",
+        component: resolve => require(["@/components/authorize"], resolve)
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: resolve => require(["@/components/login"], resolve)
+    }
   ]
 })
